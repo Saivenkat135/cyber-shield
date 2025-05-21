78e95cff213fe24d70a4fc2e1f800e353af653b6
sai venkat <saivenkatkallepalli@gmail.com>
2025-05-21 23:36:14 +0530

    created rough frontend framework

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Login from "./components/Login.jsx";
+import Main from "./components/Main.jsx";
+import AiChatBot from "./components/Hero comps/AiChatBot.jsx";
+import AboutUs from "./components/Separate Comps/AboutUs.jsx";
+import Header from "./components/Header.jsx";
+import Register from "./components/Register.jsx";
+import Rough from "./components/Rough.jsx";
+import YourProfile from "./components/YourProfie.jsx";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<YourProfile />} />
+          <Route path="/chatbot" element={<AiChatBot />} />
+          <Route
+            path="/aboutus"
+            element={
+              <div>
+                {" "}
+                <Header /> <AboutUs />{" "}
+              </div>
+            }
+          />
+          <Route path="/rough" element={<Rough />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
